478976bc45c3fad271dde22ecc13af511b73715e
Taylor Shuff <tkylesh@gmail.com>
2016-11-07 14:49:58 -0600

    added browserify. to gruntfile.js and package.json files.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,11 @@
 "use strict";
 
+// let fbAuth = require("./FirebaseAuth");
+// let fbCredentials = require("./FBCredentials");
+// let addMembers = require("./addMembers");
+// let user = require("./FBUser");
+
+// let familyMembers = {};
+// let apiKeys = {};
+// let uid = "";
+// let nfmId = 2;
