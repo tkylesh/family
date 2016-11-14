db5b05e7a83c7daad60dc1e6e95a5d6f533741e1
Taylor Shuff <tkylesh@gmail.com>
2016-11-14 15:07:42 -0600

    added initilize app to main.js.  Need to ask about sass error in grunt and possibly about dom interaction when using browserify.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,3 +2,16 @@
 
 let myFamily = require("./family");
 
+myFamily.listMembers();
+
+
+$(document).ready(()=>{
+    console.log('jquery is ready');
+    FbAPI.firebaseCredentials().then(function(keys){
+      console.log("keys", keys);
+      apiKeys = keys;
+      firebase.initializeApp(apiKeys);
+      // putTodoInDOM();
+    });
+
+});
